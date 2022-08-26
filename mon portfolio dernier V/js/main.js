e34dbcace6d2ae3cae0842f0364a316ebc57b7c8
outacht <101942312+outacht@users.noreply.github.com>
2022-08-26 18:37:00 +0200

    Add files via upload

---
+++ mon portfolio dernier V/js/main.js
@@ -0,0 +1,14 @@
+(function ($) {
+    "use strict";
+
+    // Navbar on scrolling
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.navbar').fadeIn('slow').css('display', 'flex');
+        } else {
+            $('.navbar').fadeOut('slow').css('display', 'none');
+        }
+    });
+    
+})(jQuery);
+
